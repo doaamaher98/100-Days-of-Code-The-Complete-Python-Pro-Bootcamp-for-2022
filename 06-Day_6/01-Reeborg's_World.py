8682b31ad93d567c5a318f4975fb3e5bf7019265
doaamaher <eng.doaa.m.maher@gmail.com>
2024-02-04 02:17:26 +0200

    Day(6)

---
+++ 06-Day_6/01-Reeborg's_World.py
@@ -0,0 +1,46 @@
+def turn_around() :
+    turn_left()
+    turn_left()
+
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+move()
+move()
+turn_left()
+move()
+move()
+turn_right()
+
+# Drawing a square
+def turn_around() :
+    turn_left()
+    turn_left()
+
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+'''
+move()
+turn_left()
+move()
+turn_left()
+move()
+turn_left()
+move()
+'''
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
